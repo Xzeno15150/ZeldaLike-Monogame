3ca8ee9c3df061f98f77fc316f71cb9787c9863d
Victor GAILLARD <victor.gaillard@etu.uca.fr>
2022-02-03 18:46:18 +0100

    Ajout d'une map de test

--- Assets/Grass+/Grass.tsx
+++ Assets/Grass+/Grass.tsx
@@ -1,4 +1,62 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="Grass" tilewidth="16" tileheight="16" tilecount="350" columns="25">
  <image source="GRASS+.png" trans="000000" width="400" height="224"/>
+ <tile id="285">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="286">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="3" width="16" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="287">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="3" width="16" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="288">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="3" width="15" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="311">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="2" width="16" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="325">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="8" width="15" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="327">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="15" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="328">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="331">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="16" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="349">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="15" height="16"/>
+  </objectgroup>
+ </tile>
+ <wangsets>
+  <wangset name="Jeu de Tuile sans nom" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="58" wangid="0,0,0,0,0,1,1,0"/>
+   <wangtile tileid="111" wangid="1,0,1,1,1,0,1,0"/>
+   <wangtile tileid="136" wangid="1,1,1,1,1,1,1,1"/>
+  </wangset>
+ </wangsets>
 </tileset>
